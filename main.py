0d6faca5e0166db3c7a02d00d82704eafce59d98
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-18 11:57:54 +0100

    pass in community to cell state check

--- main.py
+++ main.py
@@ -7,3 +7,11 @@
 # 2. Any live cell with two or three live neighbours lives on to the next generation.
 # 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
 # 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+class CellStateCheck():
+    def __init__(self, community):
+        self.community = community
+        return None
+    
+    def whole_community(self):
+        return self.community
--- test_cell_state.py
+++ test_cell_state.py
@@ -1,8 +1,14 @@
-from unittest import TestCase
+# from unittest import TestCase
 
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
+# class TryTesting(TestCase):
+#     def test_always_passes(self):
+#         self.assertTrue(True)
 
-    def test_always_fails(self):
-        self.assertTrue(False)
+#     def test_always_fails(self):
+#         self.assertTrue(False)
+
+from main import CellStateCheck
+
+def test_cell_exists():
+    cell = CellStateCheck([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+    assert cell.whole_community()
