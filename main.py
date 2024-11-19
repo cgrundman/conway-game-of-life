a84b3d4d36a46518534566d9eed69fd0b4832555
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-19 20:20:28 +0100

    test the padding function

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from cell_state import CellStateCheck
 
 class UpdateEnv():
     def __init__(self, env):
+        # Create Environment
         self.environment = env
+
+        # Extract size of the environment
         self.m, self.n = len(self.environment), len(self.environment[0])
+
+        # Pad the Environment
+        self.environment_padded = self.environment
+        for idx, row in enumerate(self.environment_padded):
+            self.environment_padded[idx].insert(0, 0)
+            self.environment_padded[idx].append(0)
+        self.environment_padded.insert(0, [0] * (self.n + 2))
+        self.environment_padded.append([0] * (self.n + 2))
 
         return None
     
@@ -11,4 +22,7 @@ class UpdateEnv():
         return self.environment
 
     def env_size(self):
-        return self.m, self.n
+        return len(self.environment), len(self.environment[0])
+    
+    def env_w_padding(self):
+        return self.environment_padded
--- test_update_env.py
+++ test_update_env.py
@@ -9,3 +9,9 @@ def test_env_size():
     test_env = [[1, 1],[1, 1],[1, 1]]
     test_m, test_n = UpdateEnv(test_env).env_size()
     assert test_m == len(test_env) and test_n == len(test_env[0])
+
+def test_padding():
+    test_env = [[1, 2],[3, 4],[5, 6]]
+    test_env_padded = [[0, 0, 0, 0],[0, 1, 2, 0],[0, 3, 4, 0],[0, 5, 6, 0],[0, 0, 0, 0]]
+    env_padded = UpdateEnv(test_env).env_w_padding()
+    assert env_padded == test_env_padded
