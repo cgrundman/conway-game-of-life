6407acd419ccc0d20c45c8dae319dc323f7727a8
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-19 19:04:11 +0100

    add test for pulling environment size

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from cell_state import CellStateCheck
+
 class UpdateEnv():
     def __init__(self, env):
         self.environment = env
+        self.m, self.n = len(self.environment), len(self.environment[0])
+
         return None
     
     def current_env(self):
         return self.environment
+
+    def env_size(self):
+        return self.m, self.n
--- test_update_env.py
+++ test_update_env.py
@@ -4,3 +4,8 @@ def test_env_exists():
     test_env = [[1, 1],[1, 1]]
     env = UpdateEnv(test_env).current_env()
     assert env == test_env
+
+def test_env_size():
+    test_env = [[1, 1],[1, 1],[1, 1]]
+    test_m, test_n = UpdateEnv(test_env).env_size()
+    assert test_m == len(test_env) and test_n == len(test_env[0])
