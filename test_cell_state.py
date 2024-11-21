44fe549c59199e4fd543b48daef3d2024fef856e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-21 12:52:50 +0100

    rename tests to match underlying functions being tested

--- test_cell_state.py
+++ test_cell_state.py
@@ -1,14 +1,14 @@
 from cell_state import CellStateCheck
 
-def test_community_exists():
+def test_whole_community():
     community = CellStateCheck([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
     assert community.whole_community()
 
-def test_cell_exists():
+def test_cell_only():
     community = CellStateCheck([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
     assert community.cell_only() == community.whole_community()[1][1]
 
-def test_neighborhood_exists():
+def test_neighborhood_only():
     community = CellStateCheck([[0, 0, 0],[0, 0, 0],[0, 0, 0]])
     assert len(community.neighborhood_only()) == 8
 
@@ -18,22 +18,22 @@ def test_neighborhood_value():
     # Create value to test against
     assert community.neighborhood_value() == 4
 
-def test_underpopulation():
+def test_underpopulation_new_cell_state():
     test_community = [[1, 0, 0],[0, 1, 0],[0, 0, 0]]
     community = CellStateCheck(test_community)
     assert community.new_cell_state() == 0
 
-def test_overpopulation():
+def test_overpopulation_new_cell_state():
     test_community = [[1, 1, 1],[0, 1, 0],[0, 1, 0]]
     community = CellStateCheck(test_community)
     assert community.new_cell_state() == 0
 
-def test_survival():
+def test_survival_new_cell_state():
     test_community = [[1, 1, 1],[0, 1, 0],[0, 0, 0]]
     community = CellStateCheck(test_community)
     assert community.new_cell_state() == 1
 
-def test_birth():
+def test_birth_new_cell_state():
     test_community = [[1, 1, 1],[0, 0, 0],[0, 0, 0]]
     community = CellStateCheck(test_community)
     assert community.new_cell_state() == 1
--- test_update_env.py
+++ test_update_env.py
@@ -1,6 +1,6 @@
 from main import UpdateEnv
 
-def test_env_exists():
+def test_current_env():
     test_env = [[1, 1],[1, 1]]
     env = UpdateEnv(test_env).current_env()
     assert env == test_env
@@ -10,25 +10,25 @@ def test_env_size():
     test_m, test_n = UpdateEnv(test_env).env_size()
     assert test_m == len(test_env) and test_n == len(test_env[0])
 
-def test_padding():
+def test_env_w_padding():
     test_env = [[1, 2],[3, 4],[5, 6]]
     test_env_padded = [[0, 0, 0, 0],[0, 1, 2, 0],[0, 3, 4, 0],[0, 5, 6, 0],[0, 0, 0, 0]]
     env_padded = UpdateEnv(test_env).env_w_padding()
     assert env_padded == test_env_padded
 
-def test_neighborhood_extraction():
+def test_extract_neighborhood():
     test_env = [[1, 2],[3, 4],[5, 6]]
     test_neighborhood = [[0, 0, 0],[0, 1, 2],[0, 3, 4]]
     neighborhood = UpdateEnv(test_env).extract_neighborhood()
     assert test_neighborhood == neighborhood
 
-def test_iteration():
+def test_env_iteration():
     test_env = [[1, 2],[3, 4],[5, 6]]
     iteration_order = [1, 2, 3, 4, 5, 6]
     iteration_list = UpdateEnv(test_env).env_iteration()
     assert iteration_list == iteration_order
 
-def test_implementation_1x1():
+def test_update_env_1x1():
     test_env = [[1]]
     updated_env = UpdateEnv(test_env).update_env()
     # assert updated_env == [[0]]
